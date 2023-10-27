649494add2d6a5f2dc485adb9712618e7352a80e
Bojan <bojan-stanimirovski097@hotmail.com>
2023-10-27 16:56:59 +0200

    initial setup, adding routes

--- server/index.js
+++ server/index.js
@@ -4,7 +4,23 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 require("dotenv").config();
 
+const userRoute = require("./routes/users");
+const userAuth = require("./routes/auth");
+
 const app = express();
+
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+// routes
+app.get("/", (req, res) => {
+  res.send("Hello world ");
+});
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", userAuth);
 
 const PORT = process.env.PORT;
 const mongoURI = process.env.DB_URI;
@@ -17,7 +33,3 @@ mongoose
     )
   )
   .catch((error) => console.log(`${error} MongoDB not connected`));
-
-app.listen(PORT, () => {
-  console.log(`Server Running on Port: http://localhost:${PORT}`);
-});
