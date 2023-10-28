0b4dd5926ecc8144ae3d3b59a7359cc0a75478ad
Bojan <bojan-stanimirovski097@hotmail.com>
2023-10-28 20:09:30 +0200

    Routes, added unfollowUser

--- server/routes/users.js
+++ server/routes/users.js
@@ -75,6 +75,30 @@ router.put("/:id/follow", async (req, res) => {
     res.status(403).json(`You can't follow yourself`);
   }
 });
+
 // Unfollow a user
+router.put("/:id/unfollow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $pull: { followers: req.body.userId } });
+        await currentUser.updateOne({
+          $pull: { following: req.params.id },
+        });
+
+        res.status(200).json("User has been unfollow");
+      } else {
+        return res.status(403).json("You don't follow this user");
+      }
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json(`You can't unfollow yourself`);
+  }
+});
 
 module.exports = router;
