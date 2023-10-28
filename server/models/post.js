1bde5b2d5474f0e40941ee7019eed863298e86f7
Bojan <bojan-stanimirovski097@hotmail.com>
2023-10-28 20:13:59 +0200

    Model, added postModel

---
+++ server/models/post.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required,
+    },
+    description: {
+      type: String,
+      max: 500,
+    },
+    image: {
+      type: String,
+    },
+    like: {
+      type: Array,
+      default: [],
+    },
+  },
+
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Post", postSchema);
