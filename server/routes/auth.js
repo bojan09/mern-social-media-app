037a64eb9c5ccc40b6e66067bd67c7ab1b213832
Bojan <bojan-stanimirovski097@hotmail.com>
2023-10-28 09:17:52 +0200

    Auth, added register user

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,18 @@ const router = require("express").Router();
 const User = require("../models/user");
 
 // Register
-router.post("/register", async (req, res) => {});
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    userName: req.body.userName,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  try {
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
