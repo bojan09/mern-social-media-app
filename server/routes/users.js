8721b1f934d370e08d3c078d817d6a6db65c9458
Bojan <bojan-stanimirovski097@hotmail.com>
2023-10-28 20:02:23 +0200

    Routes, added Follow a User

--- server/routes/users.js
+++ server/routes/users.js
@@ -8,8 +8,8 @@ router.get("/:id", async (req, res) => {
     const user = await User.findById(req.params.id);
     const { password, updatedAt, ...other } = user._doc;
     res.status(200).json(other);
-  } catch (error) {
-    res.status(500).json(error);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
@@ -52,6 +52,27 @@ router.delete("/:id", async (req, res) => {
 });
 
 // Follow a user
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $push: { followers: req.body.userId } });
+        await currentUser.updateOne({ $push: { following: req.body.userId } });
+
+        res.status(200).json("User has been followed");
+      } else {
+        return res.status(403).json("You already follow this user");
+      }
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json(`You can't follow yourself`);
+  }
+});
 // Unfollow a user
 
 module.exports = router;
