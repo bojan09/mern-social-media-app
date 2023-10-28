4b5180e9093e011d412d6fa8bab48ce739b6766d
Bojan <bojan-stanimirovski097@hotmail.com>
2023-10-28 09:22:22 +0200

    added Bcrypt and Password hashing

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,15 +1,23 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
 
 const User = require("../models/user");
 
 // Register
 router.post("/register", async (req, res) => {
-  const newUser = new User({
-    userName: req.body.userName,
-    email: req.body.email,
-    password: req.body.password,
-  });
   try {
+    // Generate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // Create new user
+    const newUser = new User({
+      userName: req.body.userName,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    // Save user
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (error) {
