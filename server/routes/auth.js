a6f30d2c7829ecb7ac540cdd3760bc4ba80ef124
Bojan <bojan-stanimirovski097@hotmail.com>
2023-10-28 09:48:48 +0200

    Routes: Added updateUser, added new properties in userModel

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -21,7 +21,7 @@ router.post("/register", async (req, res) => {
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (error) {
-    console.log(error);
+    res.status(500).json(error);
   }
 });
 
@@ -44,7 +44,7 @@ router.post("/login", async (req, res) => {
     // Email & Password are Valid
     res.status(200).json(user);
   } catch (error) {
-    console.log(error);
+    res.status(500).json(error);
   }
 });
 module.exports = router;
