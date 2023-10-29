d771ac51d6159848c7de01508789a2e3be8cdc40
Bojan <bojan-stanimirovski097@hotmail.com>
2023-10-29 08:21:06 +0100

    postRoute, added updatePost

--- server/routes/post.js
+++ server/routes/post.js
@@ -13,6 +13,19 @@ router.post("/", async (req, res) => {
 });
 
 // Update a Post
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("Post has been updated");
+    } else {
+      res.status(403).json("You can update only your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 // Delete a Post
 // Like a Post
 // Get a Post
