85aaa3fb645855efaac67be445288ef193e5970b
Bojan <bojan-stanimirovski097@hotmail.com>
2023-10-29 09:22:19 +0100

    postRoute, adding likePost

--- server/routes/post.js
+++ server/routes/post.js
@@ -26,8 +26,38 @@ router.put("/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
 // Delete a Post
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne({ $set: req.body });
+      res.status(200).json("Post has been deleted");
+    } else {
+      res.status(403).json("You can delete only your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // Like a Post
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("Post has been liked");
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json("Post has been disliked");
+    }
+  } catch (err) {
+    res.status.json(err);
+  }
+});
+
 // Get a Post
 // Get all Posts, by followers
 
