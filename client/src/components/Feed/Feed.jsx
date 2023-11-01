7a0d8afe478d81f91e2a6190205b540938f8c24a
Bojan <bojan-stanimirovski097@hotmail.com>
2023-11-01 21:51:19 +0100

    Test: Showing Posts in Feed

--- client/src/components/Feed/Feed.jsx
+++ client/src/components/Feed/Feed.jsx
@@ -1,13 +1,16 @@
 import "./Feed.css";
 
 import { Share, Post } from "../";
+import { Posts } from "../../dummyData.js";
 
 const Feed = () => {
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Share />
-        <Post />
+        {Posts.map((post) => (
+          <Post post={post} key={post.id} />
+        ))}
       </div>
     </div>
   );
--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -4,9 +4,10 @@ import "./Post.css";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 // asserts
-import { Heart, Like, Person_1, Post_1 } from "../../assets";
+import { Heart, Like, Person_1 } from "../../assets";
 
-const Post = () => {
+const Post = ({ post }) => {
+  console.log(post);
   return (
     <div className="post">
       <div className="postWrapper">
@@ -17,25 +18,25 @@ const Post = () => {
               alt="person"
               className="postProfileImage image"
             />
-            <span className="postUsername">Jane Doe</span>
-            <span className="postDate">5 mins ago</span>
+            <span className="postUsername">John Doe</span>
+            <span className="postDate">{post?.date}</span>
           </div>
           <div className="postTopRight">
             <MoreVertIcon />
           </div>
         </div>
         <div className="postCenter">
-          <span className="postText">Hey! It&apos;s my first post</span>
-          <img src={Post_1} alt="post_img" className="postImage" />
+          <span className="postText">{post?.description}</span>
+          <img src={post?.photo} alt="post_img" className="postImage" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
             <img className="likeIcon" src={Like} alt="like" />
             <img className="likeIcon" src={Heart} alt="heart" />
-            <span className="postLikeCounter">32 people like it</span>
+            <span className="postLikeCounter">{post?.like} people like it</span>
           </div>
           <div className="postBottomRight">
-            <span className="postCommentText">9 comments</span>
+            <span className="postCommentText">{post.comment} comments</span>
           </div>
         </div>
       </div>
